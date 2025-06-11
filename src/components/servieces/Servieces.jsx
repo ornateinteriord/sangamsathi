1035952ebc568ca131fc8146c53ab53fafa595d4
Suresh <suris6530@gmail.com>
2025-06-11 12:36:21 +0530

    aboutus

--- src/components/servieces/Servieces.jsx
+++ src/components/servieces/Servieces.jsx
@@ -11,37 +11,37 @@ import "./Servieces.scss";
 const Services = () => {
   const services = [
     {
-      icon: <LuNewspaper style={{ fontSize: "40px", color: "#fff" }} />,
+      icon: <LuNewspaper />,
       title: "SHARE YOUR RESPONSIBILITY",
       description:
         "A highly involved Relationship Manager understands your expectations and provides personalised assistance.",
     },
     {
-      icon: <IoSearchOutline style={{ fontSize: "40px", color: "#fff" }} />,
+      icon: <IoSearchOutline />,
       title: "EXPERT SEARCH WITHIN REACH",
       description:
         "Your Relationship Manager employs advanced tools and expert knowledge to find matching members from thousands of profiles.",
     },
     {
-      icon: <GrBook style={{ fontSize: "40px", color: "#fff" }} />,
+      icon: <GrBook />,
       title: "SHORTLISTED MATCHES",
       description:
         "Your Relationship Manager contacts prospects on your behalf and ensures effective communication between you and your matches.",
     },
     {
-      icon: <IoCallOutline style={{ fontSize: "40px", color: "#fff" }} />,
+      icon: <IoCallOutline />,
       title: "INITIATE COMMUNICATION",
       description:
         "Your Relationship Manager carefully handpicks profiles that are most suitable for you and sends them for your review.",
     },
     {
-      icon: <LuCoffee style={{ fontSize: "40px", color: "#fff" }} />,
+      icon: <LuCoffee />,
       title: "MEET THE PROSPECTS",
       description:
         "Your Relationship Manager contacts prospects and facilitates a meeting based on mutual interest.",
     },
     {
-      icon: <TbUsers style={{ fontSize: "40px", color: "#fff" }} />,
+      icon: <TbUsers />,
       title: "HAPPY MATCHES",
       description: "Assistance Service will be Started Shortly!",
     },
@@ -57,7 +57,7 @@ const Services = () => {
             <div className="service-card" key={index}>
               <div className="icon-wrapper">{service.icon}</div>
               <h3>{service.title}</h3>
-              <p style={{ color: '#fff' }}>{service.description}</p>
+              <p>{service.description}</p>
               {index < services.length - 1 && (
                 <FaArrowAltCircleRight className="arrow-icon" />
               )}
